f5440c6eda699130c7aa13fa1301e89df01b0353
Lucas Oliveira <oliveira.contato23@gmail.com>
2021-04-17 18:54:41 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -7,22 +7,26 @@ package gui;
 
 import application.Program;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 /**
  *
  * @author olive
  */
 public class DepartmentListController implements Initializable {
+    private DepartmentService service;
     @FXML
     private TableView<Department> tableViewDepartment;
     @FXML
@@ -32,10 +36,13 @@ public class DepartmentListController implements Initializable {
     @FXML
     private Button btNew;
     
+    private ObservableList<Department> obsList;
     public void onBtNewAction(){
     System.out.println("Button action");
     }
-    
+    public void setDepartmentService(DepartmentService service){
+    this.service = service;
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         initializeNodes();
@@ -47,5 +54,13 @@ public class DepartmentListController implements Initializable {
          Stage stage =(Stage) Program.getMainScene().getWindow();
          tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
     }
+    public void UpdateTableView(){
+        if (service == null){
+        throw new IllegalStateException("Service was null");
+        }
     
+        List<Department> list = service.findAll();
+        obsList = FXCollections.observableArrayList(list);
+        tableViewDepartment.setItems(obsList);
+    }
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -10,6 +10,7 @@ import gui.util.Alerts;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -19,6 +20,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 /**
  * FXML Controller class
@@ -41,12 +43,16 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemDepartment() {
-        loadView("/gui/DepartmentList.fxml");
+        loadView("/gui/DepartmentList.fxml", (DepartmentListController controller)->{
+            controller.setDepartmentService(new DepartmentService());
+            controller.UpdateTableView();
+        
+        });
     }
 
     @FXML
     public void onMenuItemAbout() {
-        loadView("/gui/AboutView.fxml");
+        loadView("/gui/AboutView.fxml", x -> {});
     }
 
     @Override
@@ -54,7 +60,7 @@ public class MainViewController implements Initializable {
         // TODO
     }
 
-    private synchronized void loadView(String absoluteName) {
+    private synchronized <T >void loadView(String absoluteName, Consumer<T> initializingAction) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
             VBox newVBox = loader.load();
@@ -65,9 +71,12 @@ public class MainViewController implements Initializable {
             mainVBox.getChildren().add(mainMenu);
             mainVBox.getChildren().addAll(newVBox.getChildren());
             
+            T controller = loader.getController();
+            initializingAction.accept(controller);
+            
         } catch (IOException e) {
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
         }
     }
-
+    
 }
